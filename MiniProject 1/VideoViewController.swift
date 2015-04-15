2d5017eab8983c0b4f3786c8908b3d7c94cdcbb8
Ihashi <ihashi91@yahoo.fr>
2015-04-15 15:45:06 +0200

    Use of an UIView called AVPlayerView Suppression of the buttons and view

--- MiniProject 1/VideoViewController.swift
+++ MiniProject 1/VideoViewController.swift
@@ -11,79 +11,40 @@ import AVFoundation
 
 class VideoViewController: UIViewController
 {
-    private var moviePlayer: AVPlayer?
-    private var playerLayer: AVPlayerLayer?
+    @IBOutlet weak var playerView: AVPlayerView!
     private var playerItem: AVPlayerItem?
-    private var leftButton: UIButton?
-    private var rightButton: UIButton?
-    
-    let screenSize = UIScreen.mainScreen().bounds
+    private var videoPlayer: AVPlayer?
+    private var videoNumber = 0
     private var urls = ["http://fun.siz.io/stories/142893791787803c7fb48f4d/0.mp4",
                 "http://fun.siz.io/stories/1429024267698bb127fbd1bf/0.mp4",
                 "http://fun.siz.io/stories/1429021517853eac7ce1fc29/0.mp4",
                 "http://fun.siz.io/stories/1429018976114f45db3a5f88/0.mp4"]
     
-    private var videoNumber = 0
-    
-    func displayButton()
+    @IBAction func mainButtons(sender: UIButton)
     {
-        let leftButton = UIButton.buttonWithType(UIButtonType.System) as! UIButton
-        leftButton.frame = CGRectMake(0, (screenSize.width/2)-20, 80, 40)
-        leftButton.backgroundColor = .blackColor()
-        leftButton.setTitle("Prev", forState: UIControlState.Normal)
-        leftButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        leftButton.addTarget(self, action: "previousVideo", forControlEvents: UIControlEvents.TouchUpInside)
+        let buttonName = sender.currentTitle!
         
-        let rightButton = UIButton.buttonWithType(UIButtonType.System) as! UIButton
-        rightButton.frame = CGRectMake((screenSize.height)-80, (screenSize.width/2)-20, 80, 40)
-        rightButton.backgroundColor = .blackColor()
-        rightButton.setTitle("Next", forState: UIControlState.Normal)
-        rightButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-        rightButton.addTarget(self, action: "nextVideo", forControlEvents: UIControlEvents.TouchUpInside)
-        
-        view.addSubview(leftButton)
-        view.addSubview(rightButton)
-    }
-    
-    func removePlayerAndButtons()
-    {
-        NSNotificationCenter.defaultCenter().removeObserver(self,
-            name: AVPlayerItemDidPlayToEndTimeNotification,
-            object: moviePlayer?.currentItem)
-        
-        moviePlayer?.pause()
-        playerLayer?.removeFromSuperlayer()
-        rightButton?.removeFromSuperview()
-        leftButton?.removeFromSuperview()
-    }
-    
-    func previousVideo()
-    {
-        removePlayerAndButtons()
-        
-        if(videoNumber > 0)
+        switch buttonName
         {
-            videoNumber--
-        }
-        else if(videoNumber == 0)
-        {
-            videoNumber = urls.count-1
-        }
-        
-        playVideo()
-    }
-    
-    func nextVideo()
-    {
-        removePlayerAndButtons()
-        
-        if(videoNumber < urls.count-1)
-        {
-            videoNumber++
-        }
-        else if(videoNumber == urls.count-1)
-        {
-            videoNumber = 0
+        case"PREV":
+            if(videoNumber > 0)
+            {
+                videoNumber--
+            }
+            else if(videoNumber == 0)
+            {
+                videoNumber = urls.count-1
+            }
+        case"NEXT":
+            if(videoNumber < urls.count-1)
+            {
+                videoNumber++
+            }
+            else if(videoNumber == urls.count-1)
+            {
+                videoNumber = 0
+            }
+        default: break
         }
         
         playVideo()
@@ -91,23 +52,20 @@ class VideoViewController: UIViewController
     
     func playVideo()
     {
-        var url = NSURL(string: urls[videoNumber])
+        let url = NSURL(string: urls[videoNumber])
         playerItem = AVPlayerItem(URL: url)
-        moviePlayer = AVPlayer(playerItem: playerItem)
-        playerLayer = AVPlayerLayer(player: moviePlayer)
-        moviePlayer?.actionAtItemEnd = .None
+        videoPlayer = AVPlayer(playerItem: playerItem)
+        videoPlayer?.actionAtItemEnd = .None
         
-        playerLayer?.frame = CGRect(x: 0, y: 0, width: screenSize.height, height: screenSize.width)
-        self.view.backgroundColor = .blackColor()
-        self.view.layer.addSublayer(playerLayer)
-        
-        moviePlayer?.play()
-        displayButton()
+        self.playerView.setPlayer(self.videoPlayer!)
+        self.playerView.setVideoFillMode(AVLayerVideoGravityResizeAspect)
+
+        videoPlayer?.play()
         
         NSNotificationCenter.defaultCenter().addObserver(self,
             selector: "restartVideo",
             name: AVPlayerItemDidPlayToEndTimeNotification,
-            object: moviePlayer?.currentItem)
+            object: videoPlayer?.currentItem)
     }
     
     func restartVideo()
@@ -116,18 +74,22 @@ class VideoViewController: UIViewController
         let preferredTimeScale : Int32 = 1
         let seekTime : CMTime = CMTimeMake(seconds, preferredTimeScale)
         
-        moviePlayer?.seekToTime(seekTime)
-        moviePlayer?.play()
-    }
-    
-    deinit
-    {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
+        videoPlayer?.seekToTime(seekTime)
+        videoPlayer?.play()
     }
     
     override func viewWillAppear(animated: Bool)
     {
         super.viewWillAppear(true)
         playVideo()
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    deinit
+    {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 }
